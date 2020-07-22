9be30db1ffc71748750ced2fe60926e28f9f5495
Dalena Pham <dalenapham35@gmail.com>
2020-07-22 00:37:11 -0700

    Fleshed out navbar, added stub route pages

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,24 @@
 <template>
-    <div id="nav" class="nav">
-    <router-link to="/">Character Select</router-link>
-    <nav>
-        <router-link to="/">Lost Items</router-link>
-        <router-link to="/">Lecture Questions</router-link>
-        <router-link to="/">Confessional Questions</router-link>
-    </nav>
+    <nav id="nav" class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-start">
+        <router-link to="/" class="navbar-item nes-btn pixel-font">Character Select</router-link>
+        <router-link :to="{ name: 'lost-items' }" class="navbar-item nes-btn pixel-font">Lost Items</router-link>
+        <router-link :to="{ name: 'lecture-questions' }" class="navbar-item nes-btn pixel-font">Lecture Questions</router-link>
+        <router-link :to="{ name: 'confessionals' }" class="navbar-item nes-btn pixel-font">Confessional Questions</router-link>
     </div>
+    </nav>
 </template>
 
 <script>
-export default {
+export default 
+{
     
 }
 </script>
 
 <style scoped>
-
+.nes-btn
+{
+    margin-right:10px; 
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,30 @@ const routes = [
     component: Home
   },
   {
+    path: "/character", 
+    name: "character", 
+    component: () => 
+      import("../views/Character.vue")
+  }, 
+  {
+    path: "/lostItems", 
+    name: "lost-items", 
+    component: () => 
+      import("../views/LostItems.vue")
+  },
+  {
+    path: "/lecture", 
+    name: "lecture-questions", 
+    component: () => 
+      import("../views/Lecture.vue")
+  }, 
+  {
+    path: "/confessionals", 
+    name: "confessionals", 
+    component: () => 
+      import("../views/Confessionals.vue")
+  }, 
+  {
     path: "/about",
     name: "About",
     // route level code-splitting
